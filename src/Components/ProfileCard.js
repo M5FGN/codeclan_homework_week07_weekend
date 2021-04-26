84fafc96bf5f693da1c3aff2056295f9edb01c23
Mary Fagan <m5fgn@outlook.com>
2021-04-26 20:23:41 +0100

    Add Tutor feedback - to action later

--- src/Components/ProfileCard.js
+++ src/Components/ProfileCard.js
@@ -5,6 +5,8 @@ const ProfileCard = ({userDetail}) => {
 
 return (
     <div className='profilecard'>
+
+        // TODO - need a if statment in here to show 'Loading if the API hasn't loaded yet.
         <img src={userDetail.picture.large}></img>
         <h1>{userDetail.name.first} {userDetail.name.last}</h1>
         <p>{userDetail.location.city}, {userDetail.location.country}</p>
